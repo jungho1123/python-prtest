458e3b3bab82f1e1c03a3b43445b75164bff87a3
nurungji <105197496+nyryngji@users.noreply.github.com>
2022-06-02 09:43:15 +0900

    Update python2-2.py

--- chapter2/python2-2.py
+++ chapter2/python2-2.py
@@ -2,4 +2,4 @@
 #1mile=1.61km
 km=int(input('차의 속도를 입력(km) >> '))
 mile=float(input('마일'))   #1.61입력
-print('{}(km)은 {} 마일(miles)이다.'.format(km,km/mile))
+print('{}(km)은 {} 마일(miles)이다.'.format(km,km/mile)
