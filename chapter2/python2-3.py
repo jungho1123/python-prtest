24ed6058103af14cad8b8f461268d69dcf6f445a
nurungji <105197496+nyryngji@users.noreply.github.com>
2022-06-27 16:15:49 +0900

    68p

--- chapter2/python2-3.py
+++ chapter2/python2-3.py
@@ -1,6 +1,6 @@
 #아메리카노를 주문받아 총 가격을 출력
 #아메리카노 3500원
 
-price=int(input('가격 '))
-count=int(input('아메리카노 몇 개 주문하세요? '))
-print('총 가격은 {}이다'.format(price*count))
+price=3500
+n=int(input('아메리카노 몇 개 주문하세요? '))
+print('총 가격은 {} 이다'.format(n*price))
