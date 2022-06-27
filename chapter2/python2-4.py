8ed79daa6c0e8e0e1ddb928b747ccbd9b069b41a
nurungji <105197496+nyryngji@users.noreply.github.com>
2022-06-27 16:19:21 +0900

    68p

--- chapter2/python2-4.py
+++ chapter2/python2-4.py
@@ -2,10 +2,8 @@
 #지구 둘레: 40120km, 반지름은 6378.1km
 #원둘레 공식: 2*3.141592*r(반지름)
 
-earth=int(input('지구 둘레'))
-r=float(input('반지름'))
-dule=2*3.141592*r
-
-print('알려진 지구 둘레:',earth)
-print('지구와 같은 원둘레:',dule)
-print('차이: {}km'.format(earth-dule))
+r=6378.1
+c=2*3.141592*r
+a=int(input('알려진 지구 둘레: '))
+print('지구와 같은 원둘레: {}'.format(c))
+print('차이: {}(km)'.format(a-c))
