c10d73831f62cfbcc35a967325e56705c29f2903
nurungji <105197496+nyryngji@users.noreply.github.com>
2022-06-28 22:58:06 +0900

    114p

--- chapter3/python3-1.py
+++ chapter3/python3-1.py
@@ -1,6 +1,7 @@
 #한 줄의 문자열을 표준입력으로 받아 범위, 길이, 참조문자를 출력하시오.
-str=input('문자열 입력 >> ')
-print('참조할 첨자: {}-{}'.format(0,len(str)-1))
-num=int(input('참조할 첨자 입력 >> '))
-print('문자열: {}, 길이: {}'.format(str,len(str)))
-print('참조 문자: {}'.format(str[num]))
+
+str = input('문자열 입력 >> ')
+print('참조할 첨자 : 0 ~ {}'.format(len(str)-1))
+n = int(input('참조할 첨자 입력 >> '))
+print('문자열: {}, 길이: {}'.format(str, len(str)))
+print('참조 문자: {}'.format(str[n]))
